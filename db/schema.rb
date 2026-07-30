21f8f324d6447eefd1d19c02d1c3eb9694dac0ec
Ban Alexandru Mircea <alexban011@protonmail.com>
2026-07-30 15:48:03 +0300

    Add Image model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_07_30_142231) do
+ActiveRecord::Schema[8.1].define(version: 2026_07_30_154803) do
+  create_table "images", force: :cascade do |t|
+    t.integer "byte_size"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "url"
+  end
+
   create_table "tweets", primary_key: "uuid", id: :string, force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
